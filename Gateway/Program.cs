4d3ba35420cf6e4bb259d3d9b5c8837405b74a62
Olga Nosova <nosova.olgs@gmail.com>
2022-09-11 12:54:26 +0200

    changed ocelot with yarp

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,32 +1,49 @@
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = Environment.GetEnvironmentVariable("IDENTITY_SERVER_URI")
+            ?? builder.Configuration.GetValue<string>("IdentityServer:Uri");
 
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-builder.Services.AddOcelot(builder.Configuration);
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "tweeter-api");
+    });
+});
+
+// todo: uncomment and tst if I decide to call the endpoints directly via react app
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("corsPolicy", options =>
+//    {
+//        options.WithOrigins(builder.Configuration.GetValue<string>("AllowedOrigins:Uris"));
+//    });
+//});
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseRouting();
 
-app.UseHttpsRedirection();
+//app.UseCors();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
-
-await app.UseOcelot();
+app.MapReverseProxy();
 
 app.Run();
